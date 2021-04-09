ceb822831fdb428c57e19ef024c773a20955f514
dphoanghiep1998 <dinhphuhoanghiep_t63@hus.edu.vn>
2021-04-10 00:42:48 +0700

    update

--- src/processing/dictionary.py
+++ src/processing/dictionary.py
@@ -1,6 +1,7 @@
 import os
 import json
 import xml.etree.ElementTree as ET
+import re
 # module khoi tao dictionary
 # trong dictionary co cac component(object)
 # moi component gom text va` error text(cac loi thuong gap phai cua text)
@@ -23,7 +24,7 @@ class Dictionary: # class dictionary, tao 1 class tu dien
     def load(self,list_dir): # load file text -> dict(object)
         self.words = []
         for file in os.listdir(list_dir):
-            tree = ET.parse(file)
+            tree = ET.parse(list_dir+"/"+file)
             root = tree.getroot()
             for entry in root.findall("Entry"):
                 text = entry.find("HeadWord").text
@@ -34,4 +35,13 @@ class Dictionary: # class dictionary, tao 1 class tu dien
 
 
             
+class DictionaryUtil:
+    @staticmethod
+    # co tu loi nao hay gap se dung ham nay de them vao
+    def addError(dict):
+        for word in dict.words:
+            if(re.sub('ch','tr',word.text) != ""):
+                error = re.sub('ch','tr',word.text)
+                word.set_error(error)         
+    
 
--- src/processing/module.py
+++ src/processing/module.py
@@ -1,24 +1,29 @@
 from dictionary import Dictionary
+from dictionary import DictionaryUtil
+import re
+
+ft = DictionaryUtil()
 
 dict = Dictionary()
-dict.load("xml")
+dict.load("../xml")
+ft.addError(dict)
 
 # ham check input co thoa man luat ko ( test đầu tiên với luật có trong từ điển đã)
 # để làm đc thì cần tokenizer
 # có thể có chuẩn hóa
 
-# def check(input,dict):
-#     a = input.lower().split(" ")
-#     for word in dict.words:
-#         for i in range(len(a)-1):
-#             if(a[i]+" "+a[i+1]) in word.error:
-#                     print(a[i]+" "+a[i+1])
-#                     print("True")
-# def main():
-#     if input:
-#         check(input,dict)
+def check(input,dict):
+    a = input.lower().split(" ")
+    for word in dict.words:
+        for i in range(len(a)-1):
+            if(a[i]+" "+a[i+1]) in word.error:
+                    for match in re.finditer(a[i]+" "+a[i+1],input):
+                        print([(match.start(),match.end()),a[i]+" "+a[i+1]])
+                    
+def main():
+    if input:
+        check(input,dict)
 
-# if __name__ == "__main__":
-#     input = input()
-#     main()
-print(dict.words[10])
+if __name__ == "__main__":
+    input = input()
+    main()
